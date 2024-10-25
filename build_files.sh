fbe0afe843726413390ebe4c5853623c66a0629d
MetinQardasov11 <metin.qardasov2003@gmail.com>
2024-10-25 21:57:39 +0400

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install project dependencies
 pip install -r requirements.txt
-python3.12 manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
+
+# Make migrations
+python manage.py makemigrations
+
+# Apply migrations
+python manage.py migrate
+
